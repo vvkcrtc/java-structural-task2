d6c889fe6a5a9a1e8e44567d8f630877c62ad392
vvkcrtc <105951181+vvkcrtc@users.noreply.github.com>
2022-11-07 23:22:47 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("0001", "0010"));
+        System.out.println(bins.mult("0010", "0011"));
+    }
+}
